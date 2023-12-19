eec72defdab0a34e700cbfa79f341189f3fa0470
Boris Pavic <bpavic@student.tudelft.nl>
2023-12-19 16:59:30 +0100

    Adjust whitespace

--- client/src/components/RaceThemes/RaceTheme.tsx
+++ client/src/components/RaceThemes/RaceTheme.tsx
@@ -41,8 +41,8 @@ function RaceTheme(props: Props) {
         accuracy: 100,
         animationStatus: {
             pathProgress: 0,    // initialize all ghost to progress of 0%
-                        transitionDuration: 1,  // transition duration initalized at 1, changes when updating
-                        timeScoreIndex: 0  
+            transitionDuration: 1,  // transition duration initalized at 1, changes when updating
+            timeScoreIndex: 0  
         }
     }]
 
