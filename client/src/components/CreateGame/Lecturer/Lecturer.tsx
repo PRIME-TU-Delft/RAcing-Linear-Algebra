cafb709e08e1a0430744a55a72e2d7f537dde5e5
Boris Pavic <bpavic@student.tudelft.nl>
2024-05-06 09:48:31 +0200

    Fix round timer bug

--- client/src/components/CreateGame/Lecturer/Lecturer.tsx
+++ client/src/components/CreateGame/Lecturer/Lecturer.tsx
@@ -95,8 +95,9 @@ function Lecturer(props: Props) {
     //when 10minutes count down is up
     const timeUp = () => {
         //round ends
-        socket.emit("endRound")
-        navigate("/Leaderboard")
+        // console.log("HERE")
+        // socket.emit("endRound")
+        // navigate("/Leaderboard")
     }
 
     // Give warning before refreshing page to prevent disconnecting
