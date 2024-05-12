14b39d206904b4cc3cdb640f4e60155f33c05d6a
Boris Pavic <bpavic@student.tudelft.nl>
2024-05-12 11:45:35 +0200

    Adjust question context

--- client/src/contexts/QuestionContext.ts
+++ client/src/contexts/QuestionContext.ts
@@ -1,12 +1,22 @@
 import { createContext } from 'react';
 import { IQuestion } from '../components/RaceThemes/SharedUtils';
 
-export const QuestionContext = createContext<IQuestion>({
-    question: "",
-    answer: "",
-    difficulty: "",
-    subject: "",
-    type: "",
-    options: [],
-    variants: []
+interface QuestionData {
+    iQuestion: IQuestion,
+    questionNumber: number,
+    numberOfMandatory: number
+}
+
+export const QuestionContext = createContext<QuestionData>({
+    iQuestion: {
+        question: "",
+        answer: "",
+        difficulty: "",
+        subject: "",
+        type: "",
+        options: [],
+        variants: []
+    },
+    questionNumber: 0,
+    numberOfMandatory: 0
 })
