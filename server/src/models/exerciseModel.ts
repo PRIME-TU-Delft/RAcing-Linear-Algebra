68a73ead4e0656e4c4a7a3eb21dfcd4677f48905
BorisPST <boris.pavic@gmail.com>
2024-10-08 10:46:19 +0200

    Add exercise schema

---
+++ server/src/models/exerciseModel.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+
+export interface IExercise extends mongoose.Document {
+    exerciseId: number
+    url: string
+    difficulty: string
+    numOfAttempts: number
+    name: string
+}
+
+export const exerciseSchema: mongoose.Schema = new mongoose.Schema({
+    // Grasple exercise ID
+    exerciseId: {
+        type: Number,
+        required: true,
+    },
+    // The exercise URL
+    url: {
+        type: String,
+        required: true,
+    },
+    // Exercise difficulty
+    difficulty: {
+        type: String,
+        required: true
+    },
+    // Number of attempts for the exercise
+    numOfAttempts: {
+        type: Number,
+        required: true
+    },
+    // Name of the exercise 
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+export const Exercise: mongoose.Model<IExercise> = mongoose.model<IExercise>("Exercise", exerciseSchema)
