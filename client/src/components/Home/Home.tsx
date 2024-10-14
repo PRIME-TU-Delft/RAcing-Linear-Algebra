728f66e8b2153bc348b558c23e14d8dbed40ee30
BorisPST <boris.pavic@gmail.com>
2024-10-14 22:27:51 +0200

    Add login modal

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -3,16 +3,23 @@ import "./Home.css"
 import { useNavigate } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGear } from "@fortawesome/free-solid-svg-icons"
-import { Menu } from '@mui/material';
+import { Button, Menu, Modal, TextField } from '@mui/material';
 import { MenuItem } from "@mui/material"
 
 function Home() {
     const navigate = useNavigate()
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const [lecturerLoginModalActive, setLecturerLoginModalActive] = useState<boolean>()
+    const [loggedIn, setLoggedIn] = useState<boolean>(false)
 
     const handleClick = (event: React.MouseEvent<HTMLOrSVGElement>) => {
         setAnchorEl(event.currentTarget as HTMLElement);
     };
+
+    const closeModalHandler = () => {
+        setLecturerLoginModalActive(curr => false)
+        setAnchorEl(null)
+    }
 
     return (
         <>
@@ -36,7 +43,7 @@ function Home() {
                         autoFocus={false}
                     >
                         <MenuItem 
-                            onClick={() => navigate("/LecturerPlatform")}
+                            onClick={() => setLecturerLoginModalActive(curr => true)}
                             selected={false}>
                             Lecturer platform
                         </MenuItem>
@@ -58,6 +65,45 @@ function Home() {
                     </button>
                 </div>
             </div>
+            <Modal
+                open={Boolean(lecturerLoginModalActive)}
+                onClose={closeModalHandler}
+                aria-labelledby="lecturer-login-modal"
+                aria-describedby="lecturer-login-description"
+                disableAutoFocus={true}
+            >
+                <div className="lecturer-login-modal rounded">
+                    <p id="lecturer-login-description">Only lecturers have access to the lecturer platform. Please log in with your password.</p>
+                    <form>
+                        <TextField
+                            id="password"
+                            label="Password"
+                            type="password"
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            required
+                        />
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            fullWidth
+                        >
+                            Log In
+                        </Button>
+                    </form>
+                    <Button
+                        variant="outlined"
+                        color="secondary"
+                        onClick={closeModalHandler}
+                        fullWidth
+                        style={{ marginTop: '10px' }}
+                    >
+                        Back
+                    </Button>
+                </div>
+            </Modal>
         </>
     )
 }
--- client/src/components/LecturerPlatform/LecturerPlatform.tsx
+++ client/src/components/LecturerPlatform/LecturerPlatform.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LecturerPlatform.css"
 
 function LecturerPlatform() {
+    
     return(
         <div>
-
         </div>
     )
 }
