efab5e000171e1094c0b07edf953aedbc5ff89fe
BorisPST <boris.pavic@gmail.com>
2024-10-14 21:58:14 +0200

    Add navigation to lecturer platform

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,11 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Home.css"
 import { useNavigate } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGear } from "@fortawesome/free-solid-svg-icons"
+import { Menu } from '@mui/material';
+import { MenuItem } from "@mui/material"
 
 function Home() {
     const navigate = useNavigate()
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+    const handleClick = (event: React.MouseEvent<HTMLOrSVGElement>) => {
+        setAnchorEl(event.currentTarget as HTMLElement);
+    };
 
     return (
         <>
@@ -14,9 +21,27 @@ function Home() {
                 <div className="home-title">
                     <p>RAcing Linear Algebra</p>
                 </div>
-
-                <FontAwesomeIcon icon={faGear} size="2xl" className="settings-icon"/> 
-                
+                <div className="settings-dropdown">
+                    <FontAwesomeIcon 
+                        icon={faGear} 
+                        size="2xl" 
+                        className="settings-icon" 
+                        onClick={handleClick}
+                    />
+                    <Menu
+                        id="settings-menu"
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={() => setAnchorEl(null)}
+                        autoFocus={false}
+                    >
+                        <MenuItem 
+                            onClick={() => navigate("/LecturerPlatform")}
+                            selected={false}>
+                            Lecturer platform
+                        </MenuItem>
+                    </Menu>
+                </div>
                 <div className="home-buttons">
                     <button
                         className="home-btn"
