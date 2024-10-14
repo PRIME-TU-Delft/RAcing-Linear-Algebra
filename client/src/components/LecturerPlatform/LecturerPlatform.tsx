7929cc407a84db40abe49a038ccf24d0e51e702d
BorisPST <boris.pavic@gmail.com>
2024-10-14 23:41:42 +0200

    Add header of lecturer platform

--- client/src/components/LecturerPlatform/LecturerPlatform.tsx
+++ client/src/components/LecturerPlatform/LecturerPlatform.tsx
@@ -1,24 +1,47 @@
 import React, { useEffect, useState } from "react";
-import "./LecturerPlatform.css"
-import { useNavigate } from "react-router-dom"
+import "./LecturerPlatform.css";
+import { useNavigate } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import IconButton from '@mui/material/IconButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 interface Props {
-    loggedIn: boolean
+    loggedIn: boolean;
 }
 
 function LecturerPlatform(props: Props) {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (!props.loggedIn) {
-            navigate("/")
+            // navigate("/");
         }
-    }, [props.loggedIn])
-    
-    return(
+    }, [props.loggedIn, navigate]);
+
+
+    return (
         <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div">
+                        RAcing Linear Algebra
+                    </Typography>
+                    <Tabs sx={{ flexGrow: 1 }} className="lecturer-platform-tabs">
+                        <Tab label="Topics"/>
+                        <Tab label="Exercises" />
+                    </Tabs>
+                    <IconButton color="inherit" onClick={() => navigate("/")}>
+                        <FontAwesomeIcon icon={faHome} />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
         </div>
-    )
+    );
 }
 
-export default LecturerPlatform
+export default LecturerPlatform;
