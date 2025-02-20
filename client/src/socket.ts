0f5aa7d0ca037a218880513c8870c212a1a19452
BorisPST <boris.pavic@gmail.com>
2025-02-20 19:06:31 +0100

    Add browser skip header

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,14 @@
 import { io } from "socket.io-client"
 import { host } from "./utils/APIRoutes"
 
-const socket = io(host)
+const socket = io(host, {
+    transportOptions: {
+        polling: {
+          extraHeaders: {
+            "ngrok-skip-browser-warning": "skip-browser-warning"
+          }
+        }
+    }
+})
 
 export default socket
