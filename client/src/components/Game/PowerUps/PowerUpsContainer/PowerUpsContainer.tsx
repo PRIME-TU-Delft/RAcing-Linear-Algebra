3e79f6eda35e1af1f09020b2e33249d790407899
BorisPST <boris.pavic@gmail.com>
2025-04-02 20:07:38 +0200

    Finalize container looks

--- client/src/components/Game/PowerUps/PowerUpsContainer/PowerUpsContainer.tsx
+++ client/src/components/Game/PowerUps/PowerUpsContainer/PowerUpsContainer.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./PowerUpsContainer.css"
+import PowerupsContainerIcon from "../../../../img/powerups-container-icon.png"
 
 function PowerUpsContainer() {
   return (
     <div className="power-ups-container">
+        <div className="container-symbol-element d-flex justify-content-center align-items-center">
+            <img className="symbol-image" src={PowerupsContainerIcon} alt="" />
+        </div>
     </div>
   )
 }
