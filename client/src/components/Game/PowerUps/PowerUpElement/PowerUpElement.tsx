12ae744b4c3252e0e54316b3c6a5f14bb8a0d58a
BorisPST <boris.pavic@gmail.com>
2025-04-03 20:11:51 +0200

    Temp changes

---
+++ client/src/components/Game/PowerUps/PowerUpElement/PowerUpElement.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./PowerUpElement.css";
+
+function PowerUpElement() {
+    return (
+        <div className="power-up-element">
+
+            
+        </div>
+    )
+}
+
+export default PowerUpElement;
--- client/src/components/Game/PowerUps/PowerUpsContainer/PowerUpsContainer.tsx
+++ client/src/components/Game/PowerUps/PowerUpsContainer/PowerUpsContainer.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./PowerUpsContainer.css"
 import PowerupsContainerIcon from "../../../../img/powerups-container-icon.png"
+import PowerUpElement from "../PowerUpElement/PowerUpElement";
 
 function PowerUpsContainer() {
   return (
-    <div className="power-ups-container">
+    <div className="power-ups-container d-flex">
+        <div className="container d-flex justify-content-end align-items-center">
+            <PowerUpElement/>
+            <PowerUpElement/>
+            <PowerUpElement/>
+        </div>
+
         <div className="container-symbol-element d-flex justify-content-center align-items-center">
             <img className="symbol-image" src={PowerupsContainerIcon} alt="" />
         </div>
