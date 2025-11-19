4b11869943957d840b90713f2ddc02f62645cb41
Boris Pavic <boris.pavic@gmail.com>
2025-11-19 21:27:51 +0100

    Add new sprites

--- client/src/components/RaceThemes/Sprites/BoatThemeSprites.ts
+++ client/src/components/RaceThemes/Sprites/BoatThemeSprites.ts
@@ -1,13 +1,35 @@
-import Shark from "../../../img/shark-fin.gif"
+// import Shark from "../../../img/shark-fin.gif"
 import Sun from "../../../img/sun.gif"
 import SharkReverse from "../../../img/shark-fin-reverse.gif"
 import Island from "../../../img/island-icon.png"
 import Boat from "../../../img/boat.png"
-import Lighthouse from "../../../img/Lighthouse.png"
+// import Lighthouse from "../../../img/Lighthouse.png"
 import IcebergOne from "../../../img/iceberg-1.png"
 import IcebergTwo from "../../../img/iceberg-2.png"
 import IcebergThree from "../../../img/iceberg-3.png"
 import Terrain from "../../../img/maps/boat/terrain.png"
+import Amsterdam from "../../../img/maps/boat/amsterdam.png"
+import Lighthouse from "../../../img/maps/boat/lighthouse.png"
+import LighthouseGlow from "../../../img/maps/boat/lighthouse_glow.png"
+import Dolphin from "../../../img/maps/boat/dolphin.gif"
+import Seal from "../../../img/maps/boat/seal.gif"
+import Shark from "../../../img/maps/boat/shark.png"
+import Cheese from "../../../img/maps/train/cheese.png"
+import Runway from "../../../img/maps/boat/runway.png"
+import Airplane from "../../../img/maps/boat/plane.png"
+import Deer from "../../../img/maps/boat/deer.png"
+import Boat1 from "../../../img/maps/boat/boat1.png"
+import Boat2 from "../../../img/maps/boat/boat2.png"
+import Boat3 from "../../../img/maps/boat/boat3.png"
+import Duck from "../../../img/maps/boat/duck.png"
+import Cruise from "../../../img/maps/train/cruise/cruise.gif"
+import Seagull from "../../../img/maps/train/seagull.png"
+import SeagullFly from "../../../img/maps/train/seagull_fly.gif"
+import NieuweKerk from "../../../img/maps/train/nieuwe_kerk.png"
+import ThePier from "../../../img/maps/train/the_pier.gif"
+import RotterdamBridge from "../../../img/maps/train/bridge.png"
+import SkyScraper1 from "../../../img/maps/train/skyscraper.png"
+import SkyScraper2 from "../../../img/maps/train/skyscraper2.png"
 
 export default {
     shark: Shark,
@@ -19,5 +41,25 @@ export default {
     icebergOne: IcebergOne,
     icebergTwo: IcebergTwo,
     icebergThree: IcebergThree,
-    terrain: Terrain
+    terrain: Terrain,
+    amsterdam: Amsterdam,
+    lighthouseGlow: LighthouseGlow,
+    dolphin: Dolphin,
+    seal: Seal,
+    cheese: Cheese,
+    runway: Runway,
+    airplane: Airplane,
+    deer: Deer,
+    boat1: Boat1,
+    boat2: Boat2,
+    boat3: Boat3,
+    duck: Duck,
+    cruise: Cruise,
+    seagull: Seagull,
+    seagullFly: SeagullFly,
+    nieuweKerk: NieuweKerk,
+    thePier: ThePier,
+    rotterdamBridge: RotterdamBridge,
+    skyScraper1: SkyScraper1,
+    skyScraper2: SkyScraper2
 }
