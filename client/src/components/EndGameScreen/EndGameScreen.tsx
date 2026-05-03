df4d312713e8d11593fbf8b5c8c48d0faf7cd203
BorisPST <boris.pavic@gmail.com>
2026-05-03 17:49:41 +0200

    Adjust qr code and change title

--- client/src/components/EndGameScreen/EndGameScreen.tsx
+++ client/src/components/EndGameScreen/EndGameScreen.tsx
@@ -12,52 +12,75 @@ export default function EndGameScreen() {
         ref: titleRef,
         from: { opacity: 0 },
         to: { opacity: 1 },
-    }) 
+    })
 
     const subtitleRef = useSpringRef()
     const subtitleAnimation = useSpring({
         ref: subtitleRef,
         from: { opacity: 0 },
         to: { opacity: 1 },
-    }) 
+    })
 
     const qrRef = useSpringRef()
     const qrAnimation = useSpring({
         ref: qrRef,
         from: { opacity: 0 },
         to: { opacity: 1 },
-    }) 
+    })
 
     const msgRef = useSpringRef()
     const msgAnimation = useSpring({
-        ref: qrRef,
+        ref: msgRef,
         from: { opacity: 0 },
         to: { opacity: 1 },
-    }) 
+    })
 
     const btnRef = useSpringRef()
     const btnAnimation = useSpring({
-        ref: qrRef,
+        ref: btnRef,
         from: { opacity: 0 },
         to: { opacity: 1 },
-    }) 
+    })
 
-    useChain([titleRef, subtitleRef, qrRef, msgRef, btnRef], [0, 0.25, 0.5, 0.75, 1], 4000)
+    useChain(
+        [titleRef, subtitleRef, qrRef, msgRef, btnRef],
+        [0, 0.25, 0.5, 0.75, 1],
+        4000
+    )
 
     return (
         <>
             <div className="end-game-container">
-                <a.div style={titleAnimation} className="end-game-text">We hope you enjoyed playing the game!</a.div>
-                <a.div style={subtitleAnimation} className="end-game-subtext">The following QR code takes you to a form with some questions about your experience:</a.div>
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSeT8gtxKftwHNPz9lDo0rQD_tDpw-YJrPsiWAqKVHqArtGngg/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">
-                    <a.img style={qrAnimation} src={QRCode} alt="qrcode" className="qrCode"></a.img>
+                <a.div style={titleAnimation} className="end-game-text">
+                    We hope you enjoyed playing the game!
+                </a.div>
+                <a.div style={subtitleAnimation} className="end-game-subtext">
+                    The following QR code takes you to a form with some
+                    questions about your experience:
+                </a.div>
+                <a
+                    href="https://docs.google.com/forms/d/e/1FAIpQLSeT8gtxKftwHNPz9lDo0rQD_tDpw-YJrPsiWAqKVHqArtGngg/viewform?usp=sf_link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <a.img
+                        style={qrAnimation}
+                        src={QRCode}
+                        alt="qrcode"
+                        className="qrCode"
+                    ></a.img>
                 </a>
                 <a.div style={msgAnimation} className="end-game-text2">
                     We greatly value your feedback and would appreciate it if
                     you could take a moment to share your thoughts with us!
                 </a.div>
-                <a.div style={btnAnimation} className="end-game-btn-container" >
-                    <div className="end-game-home-btn" onClick={() => navigate("/")}>Return to start</div>
+                <a.div style={btnAnimation} className="end-game-btn-container">
+                    <div
+                        className="end-game-home-btn"
+                        onClick={() => navigate("/")}
+                    >
+                        Return to start
+                    </div>
                 </a.div>
             </div>
         </>
